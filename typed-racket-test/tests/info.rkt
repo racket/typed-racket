8b3ed722059b412ae410e4a1b2cfbe4b3dd4745e
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2014-12-07 10:01:19 -0500

    Increase test timeouts for DrDr.

--- typed-racket-test/tests/info.rkt
+++ typed-racket-test/tests/info.rkt
@@ -2,6 +2,14 @@
 
 (define name "Typed Racket Test Suite")
 
+(define test-timeouts
+  '(("typed-racket/typed-racket/optimizer/run.rkt" 1200)
+    ("typed-racket/typed-racket/run.rkt" 1800)
+    ("typed-racket/typed-racket/tr-random-testing.rkt" 300)
+    ("typed-racket/typed-racket/with-tr-contracts.rkt" 1000)
+    ))
+
+
 ;; No need to compile the actual integration tests, just the harness.
 (define compile-omit-paths
   '("typed-racket/succeed"

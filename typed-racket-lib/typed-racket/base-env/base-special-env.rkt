032a11fdaef5f0ff15329c4e3472f2d2d1f566a1
Alexis King <lexi.lambda@gmail.com>
2015-01-30 17:09:50 -0800

    Add support for use-dir? param of in-directory
    Closes #21.

--- typed-racket-lib/typed-racket/base-env/base-special-env.rkt
+++ typed-racket-lib/typed-racket/base-env/base-special-env.rkt
@@ -144,7 +144,7 @@
    (->opt [-Input-Port -Symbol] (-seq -Bytes))]
   ;; in-directory
   [(make-template-identifier '*in-directory 'racket/private/for)
-   (->opt [(Un (-val #f) -Pathlike)] (-seq -Path))]
+   (->opt [(Un (-val #f) -Pathlike) (-> -Path Univ)] (-seq -Path))]
   ;; in-producer
   [(make-template-identifier 'in-producer 'racket/private/for)
    (-polydots (a b)

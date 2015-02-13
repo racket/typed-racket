47521d263efca167e82eb57659edd32c0168acc1
Kat Lyons <lyons@users.noreply.github.com>
2015-02-13 15:48:16 -0700

    Add missing type signatures for net/uri-codec

--- typed-racket-more/typed/net/uri-codec.rkt
+++ typed-racket-more/typed/net/uri-codec.rkt
@@ -6,9 +6,21 @@
   [uri-encode ( String -> String )]
   [uri-decode ( String -> String )]
 
+  [uri-path-segment-encode (-> String String)]
+  [uri-path-segment-decode (-> String String)]
+
+  [uri-userinfo-encode (-> String String)]
+  [uri-userinfo-decode (-> String String)]
+
+  [uri-unreserved-encode (-> String String)]
+  [uri-unreserved-decode (-> String String)]
+
+  [uri-path-segment-unreserved-encode (-> String String)]
+  [uri-path-segment-unreserved-decode (-> String String)]
+
   [form-urlencoded-encode ( String -> String )]
   [form-urlencoded-decode ( String -> String )]
 
   [alist->form-urlencoded ( (Listof (cons Symbol String)) -> String )]
   [form-urlencoded->alist ( String -> (Listof (cons Symbol String)) )]
-  [current-alist-separator-mode (Parameter Symbol)])
+  [current-alist-separator-mode (Parameter (U 'amp 'semi 'amp-or-semi 'semi-or-amp))])

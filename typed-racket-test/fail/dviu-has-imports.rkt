93b9390e3b0a72cf315fb2f86f35b48c9af3a441
Daniel Feltey <dfeltey@gmail.com>
2015-09-11 15:43:51 -0500

    Fix Unit integration tests to match AnyValues in error messages

--- typed-racket-test/fail/dviu-has-imports.rkt
+++ typed-racket-test/fail/dviu-has-imports.rkt
@@ -1,5 +1,5 @@
 #;
-(exn-pred (regexp-quote "type mismatch\n  expected: (Unit (import) (export) (init-depend) Any)\n  given: (Unit (import x-sig) (export) (init-depend) Integer)"))
+(exn-pred (regexp-quote "type mismatch\n  expected: (Unit (import) (export) (init-depend) AnyValues)\n  given: (Unit (import x-sig) (export) (init-depend) Integer)"))
 
 #lang typed/racket
 

e10ef1e0d4ec528cb94096a76c61ce9f09057b19
AlexKnauth <alexander@knauth.org>
2016-07-07 15:22:47 -0400

    don't throw internal errors for unreachable cast exprs
    This fills the corresponding entries in the cast table with a Dead-Code
    type so that when the contract-generation pass calls the contract-def
    thunk, it finds that in the table.

--- typed-racket-lib/typed-racket/typecheck/tc-if.rkt
+++ typed-racket-lib/typed-racket/typecheck/tc-if.rkt
@@ -1,11 +1,13 @@
 #lang racket/unit
 (require "../utils/utils.rkt"
          (rep prop-rep)
-         (types utils prop-ops)
+         (types abbrev utils prop-ops)
          (utils tc-utils)
          (typecheck signatures tc-envops tc-metafunctions)
          (types type-table)
-         racket/match)
+         (private syntax-properties)
+         racket/match
+         syntax/parse)
 
 ;; if typechecking
 (import tc-expr^)
@@ -17,13 +19,44 @@
      (define expected* (and expected (erase-props expected)))
      (define results-t
        (with-lexical-env/extend-props (list fs+)
-         #:unreachable (warn-unreachable thn)
+         #:unreachable (begin
+                         (handle-unreachable-casted-exprs thn)
+                         (warn-unreachable thn))
          (test-position-add-true tst)
          (tc-expr/check thn expected*)))
      (define results-u
        (with-lexical-env/extend-props (list fs-)
-         #:unreachable (warn-unreachable els)
+         #:unreachable (begin
+                         (handle-unreachable-casted-exprs els)
+                         (warn-unreachable els))
          (test-position-add-false tst)
          (tc-expr/check els expected*)))
 
      (merge-tc-results (list results-t results-u))]))
+
+;; handle-unreachable-casted-exprs : Any -> Void
+;; Traverses stx looking for casted-expr properties. For each one, it
+;; calls the function stored in the property, which fills an entry in
+;; the cast table with the -Dead-Code type. This is so that the
+;; contract-generation pass doesn't throw an internal error.
+(define (handle-unreachable-casted-exprs stx)
+  (syntax-parse stx
+    [(exp:casted-expr^ e)
+     ;; fill in this entry in the cast table with the -Dead-Code type
+     ((attribute exp.value) -Dead-Code)
+     (void)]
+    [stx
+     (define e (syntax-e #'stx))
+     (cond
+       [(pair? e) (handle-unreachable-casted-exprs (car e))
+                  (handle-unreachable-casted-exprs (cdr e))]
+       [(box? e) (handle-unreachable-casted-exprs (unbox e))]
+       [(vector? e) (for ([e (in-vector e)])
+                      (handle-unreachable-casted-exprs e))]
+       [(hash? e) (for ([(k v) (in-hash e)])
+                    (handle-unreachable-casted-exprs k)
+                    (handle-unreachable-casted-exprs v))]
+       [(struct? e) (for ([e (in-vector (struct->vector e))])
+                      (handle-unreachable-casted-exprs e))]
+       [else (void)])]))
+

27d13d7c8313c0da111270db87155bf1c5d69426
AlexKnauth <alexander@knauth.org>
2016-07-11 12:26:44 -0400

    any-wrap/c displays warning on opaque structures
    This includes #385 and #388, but makes it a warning instead of an error
    to pass an opaque struct as `Any` in all cases, not just in `#:opaque`
    predicates

--- typed-racket-test/xfail/pr241-variation-4.rkt
+++ typed-racket-test/xfail/pr241-variation-4.rkt
@@ -5,6 +5,8 @@
 ;; From Issue #203
 ;;   https://github.com/racket/typed-racket/issues/203
 
-(require typed-racket/utils/any-wrap)
+(require typed-racket/utils/any-wrap
+         typed-racket/utils/inspector)
+(current-inspector new-inspector)
 (struct s ())
 (contract any-wrap/c (s) 'a 'b)

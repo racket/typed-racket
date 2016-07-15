99e2eae7ce5db9578cc56e6b2512b397f2164a41
Ben Greenman <benjaminlgreenman@gmail.com>
2016-07-15 00:51:21 -0400

    fix order of and/c

--- typed-racket-lib/typed-racket/utils/hash-contract.rkt
+++ typed-racket-lib/typed-racket/utils/hash-contract.rkt
@@ -3,6 +3,6 @@
 (require racket/contract)
 (provide mutable-hash/c immutable-hash/c weak-hash/c)
 
-(define (mutable-hash/c k v) (and/c (hash/c k v #:immutable #f) (not/c hash-weak?)))
+(define (mutable-hash/c k v) (and/c hash? (not/c hash-weak?) (hash/c k v #:immutable #f)))
 (define (immutable-hash/c k v) (hash/c k v #:immutable #t))
-(define (weak-hash/c k v) (and/c (hash/c k v #:immutable #f) hash-weak?))
+(define (weak-hash/c k v) (and/c hash? hash-weak? (hash/c k v #:immutable #f)))

racket/typed-racket

commit 8e7f39025ab0ece5691cba6aea507e3bf51fc943

remove interning for most Reps in TR
Prior to this change (which was Typed Racket PR 469) all internal TR
objects (Reps) were interned and kept around for the entire duration
of type checking.  Because of this, frequent operations that rebuilt
types were particularly costly (e.g. various forms of
substitution). To recoup some of this cost, caching was being used in
a lot of places. This PR sought to remove interning as the default
behavior for Reps and allow for more flexibility in how we approach
time/space performance needs going forward.

The following changes were included in this overhaul:

Interning: All Reps are no longer interned. Right now we only intern
unions and some propositions.

Rep generic operations: we now use racket/generic so we're not
reinventing this wheel.

Singletons: Reps (e.g. TrueProp, Univ, etc) can be declared singleton,
which creates a single instance of the rep that all visible operations
(even within the declaring module) are defined in terms of
(e.g. predicates are defined as (λ (x) (eq? x singleton-instance)),
etc).

Custom constructors: Custom constructors can be specified for Reps,
which allows for simple normalization, interning, or other invariants
to be enfored whenever a Rep is created.

Union: Unions used to try to ensure no obviously overlaping types
would inhabit the same Union (e.g. (U String (Pairof Any Any) (Pairof
Int Int)) would be simplified to (U String (Pairof Any Any))). This,
however, required frequent calls to subtyping every time a Union was
modified and working with Unions thus had a high cost (another thing
that caching was used to reduce). Instead of this, Unions now enforce
a much simpler set of invariants on their members: (1) No duplicates
(by virtue of using a hash-based set), (2) Any and Nothing do not
appear in unions, and (3) Nested unions are flattened. Also, using a
hashset as the internal data structure meant that we could easily

typed-racket-lib/typed-racket/infer/intersect.rkt
@@ -1,8 +1,9 @@
 #lang racket/unit
 
-(require "../utils/utils.rkt")
-(require (rep type-rep type-mask)
-         (types abbrev base-abbrev union subtype resolve overlap)
+(require "../utils/utils.rkt"
+         (utils hset)
+         (rep type-rep type-mask rep-utils)
+         (types abbrev base-abbrev subtype resolve overlap)
          "signatures.rkt"
          racket/match
          racket/set)
@@ -15,9 +16,6 @@
 ;;  a non-additive intersection computation defined below
 ;; (i.e. only parts of t1 will remain, no parts from t2 are added))
 (define (intersect t1 t2)
-  ;; build-type: build a type while propogating bottom
-  (define (build-type constructor . args)
-    (if (memf Bottom? args) -Bottom (apply constructor args)))
   (cond
     ;; we dispatch w/ Error first, because it behaves in
     ;; strange ways (e.g. it is ⊤ and ⊥ w.r.t subtyping) and
@@ -36,11 +34,11 @@
        (define (rec t1 t2) (intersect t1 t2 seen))
        (match* (t1 t2)
          ;; quick overlap check
-         [(_ _) #:when (disjoint-masks? (Type-mask t1) (Type-mask t2)) -Bottom]
+         [(_ _) #:when (disjoint-masks? (mask t1) (mask t2)) -Bottom]
       
          ;; already a subtype
-         [(t1 t2) #:when (subtype t1 t2) t1]
-         [(t1 t2) #:when (subtype t2 t1) t2]
+         [(_ _) #:when (subtype t1 t2) t1]
+         [(_ _) #:when (subtype t2 t1) t2]
 
      
          ;; polymorphic intersect
@@ -53,32 +51,32 @@
      
          ;; structural recursion on types
          [((Pair: a1 d1) (Pair: a2 d2))
-          (build-type -pair  (rec a1 a2) (rec d1 d2))]
+          (-pair (rec a1 a2) (rec d1 d2))]
          ;; FIXME: support structural updating for structs when structs are updated to
          ;; contain not only *if* they are polymorphic, but *which* fields are too
          ;;[((Struct: _ _ _ _ _ _)
          ;; (Struct: _ _ _ _ _ _))]
          [((Syntax: t1*) (Syntax: t2*))
-          (build-type -Syntax (rec t1* t2*))]
+          (-Syntax (rec t1* t2*))]
          [((Promise: t1*) (Promise: t2*))
-          (build-type -Promise (rec t1* t2*))]
+          (-Promise (rec t1* t2*))]
 
          [((Union: t1s) t2)
           (match t2
             ;; let's be consistent in slamming together unions
             ;; (i.e. if we don't do this dual traversal, the order the
             ;; unions are passed to 'intersect' can produces different
-            ;; albeit equivalent (modulo subtyping, we hope) answers)
-            [(Union: t2s) (apply Un (for*/list ([t1 (in-list t1s)]
-                                                [t2 (in-list t2s)])
-                                      (rec t1 t2)))]
-            [_ (apply Un (map (λ (t1) (rec t1 t2)) t1s))])]
-         [(t1 (Union: t2s)) (apply Un (map (λ (t2) (rec t1 t2)) t2s))]
+            ;; (albeit equivalent modulo subtyping, we believe) answers)
+            [(Union: t2s) (make-Union (for*/hset ([t1 (in-hset t1s)]
+                                                  [t2 (in-hset t2s)])
+                                                 (rec t1 t2)))]
+            [_ (Union-map t1s (λ (t1) (rec t1 t2)))])]
+         [(t1 (Union: t2s)) (Union-map t2s (λ (t2) (rec t1 t2)))]
 
          [((Intersection: t1s) t2)
-          (apply -unsafe-intersect (map (λ (t1) (rec t1 t2)) t1s))]
+          (apply -unsafe-intersect (hset-map t1s (λ (t1) (rec t1 t2))))]
          [(t1 (Intersection: t2s))
-          (apply -unsafe-intersect (map (λ (t2) (rec t1 t2)) t2s))]
+          (apply -unsafe-intersect (hset-map t2s (λ (t2) (rec t1 t2))))]
 
          ;; For resolvable types, we record the occurrence and save a back pointer
          ;; in 'seen'. Then, if this pair of types emerges again, we know that we are
@@ -166,10 +164,10 @@
       
       ;; unions
       [((Union: t1s) t2)
-       (apply Un (map (λ (t1) (restrict t1 t2 resolved)) t1s))]
+       (Union-map  t1s (λ (t1) (restrict t1 t2 resolved)))]
 
       [(t1 (Union: t2s))
-       (apply Un (map (λ (t2) (restrict t1 t2 resolved)) t2s))]
+       (Union-map t2s (λ (t2) (restrict t1 t2 resolved)))]
       
       ;; restrictions
       [((Intersection: t1s) t2)

typed-racket-lib/typed-racket/types/base-abbrev.rkt
@@ -6,7 +6,8 @@
 ;; extends it with more types and type abbreviations.
 
 (require "../utils/utils.rkt"
-         (rep type-rep prop-rep object-rep values-rep rep-utils)
+         "../rep/type-rep.rkt"
+         (rep prop-rep object-rep values-rep rep-utils)
          (env mvar-env)
          racket/match racket/list (prefix-in c: (contract-req))
          (for-syntax racket/base syntax/parse racket/list)
@@ -17,40 +18,21 @@
          -is-type
          -not-type
          -id-path
+         (all-from-out "../rep/type-rep.rkt")
          (rename-out [make-Listof -lst]
                      [make-MListof -mlst]))
 
-;; This table maps types (or really, the sequence number of the type)
-;; to identifiers that are those types. This allows us to avoid
-;; reconstructing the type when using it from its marshaled
-;; representation.  The table is referenced in env/init-env.rkt
-;;
-;; For example, instead of marshalling a big union for `Integer`, we
-;; simply emit `-Integer`, which evaluates to the right type.
-(define predefined-type-table (make-hasheq))
-(define-syntax-rule (declare-predefined-type! id)
-  (hash-set! predefined-type-table (Rep-seq id) #'id))
-(provide predefined-type-table)
-(define-syntax-rule (define/decl id e)
-  (begin (define id e)
-	 (declare-predefined-type! id)))
-
-;; Top and error types
-(define/decl Univ (make-Univ))
-(define/decl -Bottom (make-Bottom))
-(define/decl Err (make-Error))
-
 (define/decl -False (make-Value #f))
 (define/decl -True (make-Value #t))
-(define/decl -Boolean (make-Union (list -False -True)))
+(define/decl -Boolean (Un -False -True))
 
 (define -val make-Value)
+(define/decl -Null (-val null))
 
 ;; Char type and List type (needed because of how sequences are checked in subtype)
 (define/decl -Char (make-Base 'Char #'char? char? #f))
-(define/decl -Null (-val null))
-(define (make-Listof elem) (-mu list-rec (simple-Un -Null (make-Pair elem list-rec))))
-(define (make-MListof elem) (-mu list-rec (simple-Un -Null (make-MPair elem list-rec))))
+(define (make-Listof elem) (-mu list-rec (Un -Null (make-Pair elem list-rec))))
+(define (make-MListof elem) (-mu list-rec (Un -Null (make-MPair elem list-rec))))
 
 ;; Needed for evt checking in subtype.rkt
 (define/decl -Symbol (make-Base 'Symbol #'symbol? symbol? #f))
@@ -67,21 +49,6 @@
   (-Tuple* l -Null))
 (define (-Tuple* l b)
   (foldr -pair b l))
-
-;; Simple union type constructor, does not check for overlaps
-;; Normalizes representation by sorting types.
-;; Type * -> Type
-;; The input types can be union types, but should not have a complicated
-;; overlap relationship.
-(define simple-Un
-  (let ([flat (match-lambda
-                [(Union: es) es]
-                [t (list t)])])
-    (case-lambda
-      [() -Bottom]
-      [(t) t]
-      [args
-       (make-Union (remove-duplicates (append-map flat args) type-equal?))])))
 
 ;; Recursive types
 (define-syntax -v
@@ -104,11 +71,8 @@
      (make-Result t pset o)]))
 
 ;; Propositions
-(define/decl -tt (make-TrueProp))
-(define/decl -ff (make-FalseProp))
 (define/decl -tt-propset (make-PropSet -tt -tt))
 (define/decl -ff-propset (make-PropSet -ff -ff))
-(define/decl -empty-obj (make-Empty))
 
 (define (-arg-path arg [depth 0])
   (make-Path null (cons depth arg)))

typed-racket-lib/typed-racket/types/update.rkt
@@ -5,8 +5,8 @@
          (contract-req)
          (infer-in infer)
          (rep core-rep type-rep prop-rep object-rep values-rep rep-utils)
-         (utils tc-utils)
-         (types resolve subtype subtract union)
+         (utils tc-utils hset)
+         (types resolve subtype subtract)
          (rename-in (types abbrev)
                     [-> -->]
                     [->* -->*]
@@ -82,11 +82,11 @@
            (list (make-arr* doms (update rng rst))))]
          
          [((Union: ts) _)
-          (apply Un (map (λ (t) (update t path)) ts))]
+          (Union-map ts (λ (t) (update t path)))]
 
          [((Intersection: ts) _)
           (for/fold ([t Univ])
-                    ([elem (in-list ts)])
+                    ([elem (in-hset ts)])
             (intersect t (update elem path)))]
          
          [(_ _)

typed-racket-test/fail/issue-169-1.rkt
@@ -1,5 +1,5 @@
 #;
-(exn-pred #rx"expected: \\(Listof Nothing\\).*given: \\(Listof Pos\\*\\)")
+(exn-pred #rx"expected: \\Null.*given: \\(Listof Pos\\*\\)")
 #lang typed/racket
 (define-type Pos Integer)
 (define-new-subtype Pos* (p Pos))

typed-racket-test/fail/issue-169-2.rkt
@@ -1,5 +1,5 @@
 #;
-(exn-pred #rx"expected: \\(Listof Nothing\\).*given: \\(Listof Pos\\*\\)")
+(exn-pred #rx"expected: \\Null.*given: \\(Listof Pos\\*\\)")
 #lang typed/racket
 (define-type Pos Integer)
 (define-new-subtype Pos* (p Pos))

commit da30fb6f740b28a58ba901a1ae44600f99bc4eaf

be less eager about bottom propogation
Moving to eager propagating of bottom works for most cases,
but in some cases flattening types such as (Pairof Bottom Any)
to Bottom made things like type inference break for some cases
(since (Listof Nothing) == Null, and (Listof A) did not structurally
like up like it used to). Perhaps w/ a little more effort
inference and any other potential issues could work better
with propagating bottom, but for now we'll be slightly less
aggressive about it.

i.e. this fixes pfds, which commit 8e7f390 broke.

typed-racket-lib/typed-racket/infer/intersect.rkt
@@ -51,15 +51,15 @@
      
          ;; structural recursion on types
          [((Pair: a1 d1) (Pair: a2 d2))
-          (-pair (rec a1 a2) (rec d1 d2))]
+          (rebuild -pair (rec a1 a2) (rec d1 d2))]
          ;; FIXME: support structural updating for structs when structs are updated to
          ;; contain not only *if* they are polymorphic, but *which* fields are too
          ;;[((Struct: _ _ _ _ _ _)
          ;; (Struct: _ _ _ _ _ _))]
          [((Syntax: t1*) (Syntax: t2*))
-          (-Syntax (rec t1* t2*))]
+          (rebuild -Syntax (rec t1* t2*))]
          [((Promise: t1*) (Promise: t2*))
-          (-Promise (rec t1* t2*))]
+          (rebuild -Promise (rec t1* t2*))]
 
          [((Union: t1s) t2)
           (match t2
@@ -130,9 +130,6 @@
 ;; and sometimes we want to make sure and _not_ add t2 to the result
 ;; we just want to keep the parts of t1 consistent with t2)
 (define (restrict t1 t2)
-  ;; build-type: build a type while propogating bottom
-  (define (build-type constructor . args)
-    (if (memf Bottom? args) -Bottom (apply constructor args)))
   ;; resolved is a set tracking previously seen restrict cases
   ;; (i.e. pairs of t1 t2) to prevent infinite unfolding.
   ;; subtyping performs a similar check for the same
@@ -150,17 +147,17 @@
       
       ;; structural recursion on types
       [((Pair: a1 d1) (Pair: a2 d2)) 
-       (build-type -pair 
-                   (restrict a1 a2 resolved) 
-                   (restrict d1 d2 resolved))]
+       (rebuild -pair
+                (restrict a1 a2 resolved)
+                (restrict d1 d2 resolved))]
       ;; FIXME: support structural updating for structs when structs are updated to
       ;; contain not only *if* they are polymorphic, but *which* fields are too  
       ;;[((Struct: _ _ _ _ _ _)
       ;; (Struct: _ _ _ _ _ _))]
       [((Syntax: t1*) (Syntax: t2*))
-       (build-type -Syntax (restrict t1* t2* resolved))]
+       (rebuild -Syntax (restrict t1* t2* resolved))]
       [((Promise: t1*) (Promise: t2*))
-       (build-type -Promise (restrict t1* t2* resolved))]
+       (rebuild -Promise (restrict t1* t2* resolved))]
       
       ;; unions
       [((Union: t1s) t2)

typed-racket-lib/typed-racket/rep/type-rep.rkt
@@ -199,11 +199,7 @@
 ;; left and right are Types
 (def-structural Pair ([left #:covariant]
                       [right #:covariant])
-  [#:mask mask:pair]
-  [#:custom-constructor
-   (if (or (Bottom? left) (Bottom? right))
-       -Bottom
-       (make-Pair left right))])
+  [#:mask mask:pair])
 
 ;;----------------
 ;; Mutable Pairs
@@ -216,11 +212,7 @@
 ;; *mutable* pairs - distinct from regular pairs
 ;; left and right are Types
 (def-structural MPair ([left #:invariant] [right #:invariant])
-  [#:mask mask:mpair]
-  [#:custom-constructor
-   (if (or (Bottom? left) (Bottom? right))
-       -Bottom
-       (make-MPair left right))])
+  [#:mask mask:mpair])
 
 ;;----------
 ;; Vectors
@@ -242,11 +234,7 @@
   [#:singleton -BoxTop])
 
 (def-structural Box ([elem #:invariant])
-  [#:mask mask:box]
-  [#:custom-constructor
-   (if (Bottom? elem)
-       -Bottom
-       (make-Box elem))])
+  [#:mask mask:box])
 
 ;;----------
 ;; Channel
@@ -257,11 +245,7 @@
   [#:singleton -ChannelTop])
 
 (def-structural Channel ([elem #:invariant])
-  [#:mask mask:channel]
-  [#:custom-constructor
-   (if (Bottom? elem)
-       -Bottom
-       (make-Channel elem))])
+  [#:mask mask:channel])
 
 ;;----------------
 ;; Async-Channel
@@ -272,11 +256,7 @@
   [#:singleton -Async-ChannelTop])
 
 (def-structural Async-Channel ([elem #:invariant])
-  [#:mask mask:channel]
-  [#:custom-constructor
-   (if (Bottom? elem)
-       -Bottom
-       (make-Async-Channel elem))])
+  [#:mask mask:channel])
 
 ;;-------------
 ;; ThreadCell
@@ -287,33 +267,21 @@
   [#:singleton -ThreadCellTop])
 
 (def-structural ThreadCell ([elem #:invariant])
-  [#:mask mask:thread-cell]
-  [#:custom-constructor
-   (if (Bottom? elem)
-       -Bottom
-       (make-ThreadCell elem))])
+  [#:mask mask:thread-cell])
 
 ;;----------
 ;; Promise
 ;;----------
 
 (def-structural Promise ([elem #:covariant])
-  [#:mask mask:promise]
-  [#:custom-constructor
-   (if (Bottom? elem)
-       -Bottom
-       (make-Promise elem))])
+  [#:mask mask:promise])
 
 ;;------------
 ;; Ephemeron
 ;;------------
 
 (def-structural Ephemeron ([elem #:covariant])
-  [#:mask mask:ephemeron]
-  [#:custom-constructor
-   (if (Bottom? elem)
-       -Bottom
-       (make-Ephemeron elem))])
+  [#:mask mask:ephemeron])
 
 
 ;;-----------
@@ -325,11 +293,7 @@
   [#:singleton -Weak-BoxTop])
 
 (def-structural Weak-Box ([elem #:invariant])
-  [#:mask mask:other-box]
-  [#:custom-constructor
-   (if (Bottom? elem)
-       -Bottom
-       (make-Weak-Box elem))])
+  [#:mask mask:other-box])
 
 
 ;;---------------
@@ -337,11 +301,7 @@
 ;;---------------
 
 (def-structural CustodianBox ([elem #:covariant])
-  [#:mask mask:other-box]
-  [#:custom-constructor
-   (if (Bottom? elem)
-       -Bottom
-       (make-CustodianBox elem))])
+  [#:mask mask:other-box])
 
 ;;------
 ;; Set
@@ -385,22 +345,14 @@
 
 ;; t is the type of the result of syntax-e, not the result of syntax->datum
 (def-structural Syntax ([t #:covariant])
-  [#:mask mask:syntax]
-  [#:custom-constructor
-   (if (Bottom? t)
-       -Bottom
-       (make-Syntax t))])
+  [#:mask mask:syntax])
 
 ;;---------
 ;; Future
 ;;---------
 
 (def-structural Future ([t #:covariant])
-  [#:mask mask:future]
-  [#:custom-constructor
-   (if (Bottom? t)
-       -Bottom
-       (make-Future t))])
+  [#:mask mask:future])
 
 
 ;;---------------
@@ -417,11 +369,7 @@
 ;;   and the codomains of `handler`
 (def-structural Prompt-Tagof ([body #:invariant]
                               [handler #:invariant])
-  [#:mask mask:prompt-tag]
-  [#:custom-constructor
-   (if (or (Bottom? body) (Bottom? handler))
-       -Bottom
-       (make-Prompt-Tagof body handler))])
+  [#:mask mask:prompt-tag])
 
 ;;--------------------------
 ;; Continuation-Mark-Keyof
@@ -460,11 +408,7 @@
   [#:frees (f) (make-invariant (combine-frees (map f elems)))]
   [#:fmap (f) (make-HeterogeneousVector (map f elems))]
   [#:for-each (f) (for-each f elems)]
-  [#:mask mask:vector]
-  [#:custom-constructor
-   (if (ormap Bottom? elems)
-       -Bottom
-       (make-HeterogeneousVector elems))])
+  [#:mask mask:vector])
 
 
 ;; * * * * * * *
@@ -617,14 +561,12 @@
   ;; This should eventually be based on understanding of struct properties.
   [#:mask (mask-union mask:struct mask:procedure)]
   [#:custom-constructor
-   (cond
-     [(ormap (λ (fld) (Bottom? (fld-t fld))) flds) -Bottom]
-     [else (make-Struct (normalize-id name)
-                        parent
-                        flds
-                        proc
-                        poly?
-                        (normalize-id pred-id))])])
+   (make-Struct (normalize-id name)
+                parent
+                flds
+                proc
+                poly?
+                (normalize-id pred-id))])
 
 ;; Represents prefab structs
 ;; key  : prefab key encoding mutability, auto-fields, etc.
@@ -634,11 +576,7 @@
   [#:frees (f) (combine-frees (map f flds))]
   [#:fmap (f) (make-Prefab key (map f flds))]
   [#:for-each (f) (for-each f flds)]
-  [#:mask mask:prefab]
-  [#:custom-constructor
-   (cond
-     [(ormap Bottom? flds) -Bottom]
-     [else (make-Prefab key flds)])])
+  [#:mask mask:prefab])
 
 (def-type StructTypeTop ()
   [#:mask mask:struct-type]
@@ -772,10 +710,7 @@
   [#:fmap (f) (make-Refinement (f parent) pred)]
   [#:for-each (f) (f parent)]
   [#:mask (λ (t) (mask (Refinement-parent t)))]
-  [#:custom-constructor
-   (if (Bottom? parent)
-       -Bottom
-       (make-Refinement parent (normalize-id pred)))])
+  [#:custom-constructor (make-Refinement parent (normalize-id pred))])
 
 ;; A Row used in type instantiation
 ;; For now, this should not appear in user code. It's used

typed-racket-lib/typed-racket/types/base-abbrev.rkt
@@ -22,6 +22,16 @@
          (rename-out [make-Listof -lst]
                      [make-MListof -mlst]))
 
+(define-syntax (rebuild stx)
+  (syntax-case stx ()
+    [(_ mk args ...)
+     (with-syntax ([(t ...) (generate-temporaries #'(args ...))])
+       (syntax/loc stx
+         (let ([t args] ...)
+           (if (or (Bottom? t) ...)
+               -Bottom
+               (mk t ...)))))]))
+
 (define/decl -False (make-Value #f))
 (define/decl -True (make-Value #t))
 (define/decl -Boolean (Un -False -True))

typed-racket-lib/typed-racket/types/update.rkt
@@ -23,9 +23,6 @@
 ;;   in *syntactic order* (e.g. (car (cdr x)) -> '(car cdr))  
 (define/cond-contract (update t new-t pos? path-elems)
   (Type? Type? boolean? (listof PathElem?) . -> . Type?)
-  ;; build-type: build a type while propogating bottom
-  (define (build constructor . args)
-    (if (memf Bottom? args) -Bottom (apply constructor args)))
   ;; update's inner recursive loop
   ;; puts path in *accessed* order
   ;; (i.e. (car (cdr x)) --> (list cdr car))
@@ -38,15 +35,15 @@
        (match* ((resolve t) path-elem)
          ;; pair ops
          [((Pair: t s) (CarPE:))
-          (build -pair (update t rst) s)]
+          (rebuild -pair (update t rst) s)]
          [((Pair: t s) (CdrPE:))
-          (build -pair t (update s rst))]
+          (rebuild -pair t (update s rst))]
          ;; syntax ops
          [((Syntax: t) (SyntaxPE:))
-          (build -Syntax (update t rst))]
+          (rebuild -Syntax (update t rst))]
          ;; promise op
          [((Promise: t) (ForcePE:))
-          (build -Promise (update t rst))]
+          (rebuild -Promise (update t rst))]
          
          ;; struct ops
          [((Struct: nm par flds proc poly pred)

typed-racket-test/fail/issue-169-1.rkt
@@ -1,5 +1,5 @@
 #;
-(exn-pred #rx"expected: \\Null.*given: \\(Listof Pos\\*\\)")
+(exn-pred #rx"expected: \\(Listof Nothing\\).*given: \\(Listof Pos\\*\\)")
 #lang typed/racket
 (define-type Pos Integer)
 (define-new-subtype Pos* (p Pos))

typed-racket-test/fail/issue-169-2.rkt
@@ -1,5 +1,5 @@
 #;
-(exn-pred #rx"expected: \\Null.*given: \\(Listof Pos\\*\\)")
+(exn-pred #rx"expected: \\(Listof Nothing\\).*given: \\(Listof Pos\\*\\)")
 #lang typed/racket
 (define-type Pos Integer)
 (define-new-subtype Pos* (p Pos))

typed-racket-test/succeed/inf-nested-bot.rkt
@@ -1,0 +1,16 @@
+#lang typed/racket
+
+(define-type (Promiseof A) (Boxof (U (→ (Listof A)) (Listof A))))
+(struct: (A) Queue ([fld : (Promiseof A)]))
+
+(define-syntax-rule (empty A)
+  ((inst Queue A) (box (lambda: () '()))))
+
+(: empty? : (All (A) ((Queue A) -> Boolean)))
+(define (empty? q)
+  (null? (Queue-fld q)))
+
+;; make sure that inference works on nested 'Nothing' types
+(empty? (empty Nothing))
+
+
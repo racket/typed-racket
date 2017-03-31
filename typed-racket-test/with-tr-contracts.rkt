bcc5c26ca80e753647d1d0a86b66d205bd818091
Andrew Kent <andmkent@iu.edu>
2017-03-31 07:03:24 -0400

    disable with-tr-contracts for now
    Running with-tr-contracts as-is takes an unreasonable
    amount of time because the tests are run w/o recompiling
    typed racket. We need to find a way to make this more
    reasonable (i.e. make sure we recompile before running
    the unit tests) before enabling them for DrDr again.

--- typed-racket-test/with-tr-contracts.rkt
+++ typed-racket-test/with-tr-contracts.rkt
@@ -1,10 +1,11 @@
 #lang racket
 
-(void (putenv "PLT_TR_CONTRACTS" "1"))
+#;(void (putenv "PLT_TR_CONTRACTS" "1"))
 
 (define ns (make-base-namespace))
 (current-namespace ns)
 (use-compiled-file-paths null)
 
+#;
 ((dynamic-require 'typed-racket-test/main 'go/text)
  (dynamic-require 'typed-racket-test/main 'unit-tests))

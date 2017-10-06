6cffbfa6d827b22a41a5b9daad7399c8c49f083a
Ben Greenman <benjaminlgreenman@gmail.com>
2017-10-06 15:18:47 -0400

    checkpoint: example fix for msgpack failure

--- typed-racket-lib/typed-racket/utils/hash-contract.rkt
+++ typed-racket-lib/typed-racket/utils/hash-contract.rkt
@@ -1,8 +1,18 @@
 #lang racket/base
 
 (require racket/contract)
-(provide mutable-hash/c immutable-hash/c weak-hash/c)
+(provide tr-hash/c mutable-hash/c immutable-hash/c weak-hash/c)
 
-(define (mutable-hash/c k v) (and/c hash? (not/c hash-weak?) (hash/c k v #:immutable #f)))
-(define (immutable-hash/c k v) (hash/c k v #:immutable #t))
-(define (weak-hash/c k v) (and/c hash? hash-weak? (hash/c k v #:immutable #f)))
+(define (mutable-hash/c k v)
+  (and/c hash? (not/c hash-weak?) (tr-hash/c k v #:immutable #f)))
+
+(define (immutable-hash/c k v)
+  (tr-hash/c k v #:immutable #t))
+
+(define (weak-hash/c k v)
+  (and/c hash? hash-weak? (tr-hash/c k v #:immutable #f)))
+
+(define (tr-hash/c k v #:immutable [immutable 'dont-care])
+  (if (flat-contract? k)
+    (hash/c k v #:immutable immutable)
+    (and/c hash? hash-equal? (hash/c k v #:immutable immutable))))

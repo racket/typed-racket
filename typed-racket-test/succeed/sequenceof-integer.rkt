9fe2252eef8742fba2813895e56d9f9cd67b673e
Ben Greenman <benjaminlgreenman@gmail.com>
2017-11-08 21:04:27 -0500

    add regression test for (Sequenceof T)
    Test to make sure `(Sequenceof (Boxof T))` wraps sequence elements in a contract

--- typed-racket-test/succeed/sequenceof-integer.rkt
+++ typed-racket-test/succeed/sequenceof-integer.rkt
@@ -13,7 +13,15 @@
   (require (submod ".." typed))
   (define (bar) (foo 0)))
 
+(module contract-test typed/racket/base
+  (define b* : (Sequenceof (Boxof Integer)) (list (box 0)))
+  (provide b*))
+
 (require 'typed
-         'other-typed)
-(foo 0)
-(bar)
+         'other-typed
+         'contract-test
+         rackunit)
+(check-equal? (foo 0) "I got an integer: 0")
+(check-equal? (bar) "I got an integer: 0")
+(check-exn exn:fail:contract?
+  (λ () (set-box! (car b*) 'NaN)))

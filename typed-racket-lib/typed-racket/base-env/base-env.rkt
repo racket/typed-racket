68eb4e1fb520ad01bc21374e18af61bdef41d281
Philip McGrath <LiberalArtist@users.noreply.github.com>
2018-05-24 21:19:49 -0500

    Fix type of parse-command-line (#694)
    * Fix type of parse-command-line
    
    1. Fix incorrect type for `'help-labels` specification
    2. Add type for `'usage-help` specification
    3. Support optional "help-proc" and "unknown-proc" arguments
    4. Use more specific types for "finish-proc" and the flag specs
    
    Closes #692

--- typed-racket-lib/typed-racket/base-env/base-env.rkt
+++ typed-racket-lib/typed-racket/base-env/base-env.rkt
@@ -3074,15 +3074,25 @@
 
 ;; Section 15.9 (racket/cmdline)
 [parse-command-line
- (let ([mode-sym (one-of/c 'once-each 'once-any 'multi 'final 'help-labels)])
-   (-polydots (b a)
-              (cl->* (-Pathlike
-                      (Un (-lst -String) (-vec -String))
-                      (-lst (Un (-pair mode-sym (-lst (-lst Univ)))
-                                (-pair (-val 'ps) (-lst -String))))
-                      ((list Univ) [a a] . ->... . b)
-                      (-lst -String)
-                      . -> . b))))]
+ (let ([mode-sym (one-of/c 'once-each 'once-any 'multi 'final)]
+       [label-sym (one-of/c 'ps 'help-labels 'usage-help)])
+   (-polydots
+    (b a) 
+    (cl->* (->opt -Pathlike
+                  (Un (-lst -String) (-vec -String))
+                  (-lst (Un (-pair mode-sym
+                                   ;; With the `command-line` macro, the typechecker
+                                   ;; can't figure out that a type specifying the shape of
+                                   ;; the flag specification list would be satisfied.
+                                   (-lst Univ))
+                            (-pair label-sym
+                                   (-lst -String))))
+                  (->... (list (-lst Univ)) [-String a] b)
+                  (-lst -String)
+                  [(-> -String Univ)
+                   ;; Still permits unknown-proc args that accept rest arguments
+                   (-> -String Univ)]
+                  b))))] 
 
 ;; Section 16.1 (Weak Boxes)
 [make-weak-box (-poly (a) (-> a (-weak-box a)))]

7588f2346d4d27181b544daf1939a41856a78200
Joel Dueck <joel@jdueck.net>
2019-11-07 08:17:32 -0600

    Correct type of file/md5 (#2)

--- typed-racket-lib/typed/file/md5.rkt
+++ typed-racket-lib/typed/file/md5.rkt
@@ -1,4 +1,5 @@
 #lang typed/racket/base
 (require/typed file/md5
-               [md5 ((U Bytes Input-Port) -> Bytes)])
+               [md5 (case-> [(U Input-Port Bytes String) -> Bytes]
+                            [(U Input-Port Bytes String) Boolean -> Bytes])])
 (provide md5)

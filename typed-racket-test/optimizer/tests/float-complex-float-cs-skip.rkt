299ee8b27900f04d84a1bed14ee52f7e208d74d4
Sam Tobin-Hochstadt <samth@indiana.edu>
2019-12-16 17:23:59 -0500

    Abandon optimization of float-complex division.
    This is the hardest-to-maintain part of the optimizer, and I now
    bow to @stamourv's wisdom that it's not worth it.

--- typed-racket-test/optimizer/tests/float-complex-float-cs-skip.rkt
+++ typed-racket-test/optimizer/tests/float-complex-float-cs-skip.rkt
@@ -1,6 +1,7 @@
 #;#;
 #<<END
 TR info: float-complex-float-cs-skip.rkt 7:3 printf -- hidden parameter
+TR missed opt: float-complex-float-cs-skip.rkt 27:65 (make-rectangular 4 1.7976931348623157e+308) -- generic comparison -- caused by: 27:83 4
 TR opt: float-complex-float-cs-skip.rkt 15:0 (+ 1.0+2.0i 2.0 3.0+6.0i) -- unboxed binary float complex
 TR opt: float-complex-float-cs-skip.rkt 15:12 2.0 -- float in complex ops
 TR opt: float-complex-float-cs-skip.rkt 15:16 3.0+6.0i -- unboxed literal
@@ -17,19 +18,13 @@ TR opt: float-complex-float-cs-skip.rkt 18:0 (- 1.0+2.0i 2.0+4.0i 3.0) -- unboxe
 TR opt: float-complex-float-cs-skip.rkt 18:12 2.0+4.0i -- unboxed literal
 TR opt: float-complex-float-cs-skip.rkt 18:21 3.0 -- float in complex ops
 TR opt: float-complex-float-cs-skip.rkt 18:3 1.0+2.0i -- unboxed literal
-TR opt: float-complex-float-cs-skip.rkt 19:0 (/ 0.0 +inf.0-1.0i) -- unboxed binary float complex
-TR opt: float-complex-float-cs-skip.rkt 19:3 0.0 -- float in complex ops
-TR opt: float-complex-float-cs-skip.rkt 19:7 +inf.0-1.0i -- unboxed literal
 TR opt: float-complex-float-cs-skip.rkt 20:26 3/2 -- non float complex in complex ops
 TR opt: float-complex-float-cs-skip.rkt 20:3 (* -0.9263371220283309 3/2 (make-rectangular +inf.0 0.7692234292042541)) -- unboxed binary float complex
 TR opt: float-complex-float-cs-skip.rkt 20:30 (make-rectangular +inf.0 0.7692234292042541) -- make-rectangular elimination
 TR opt: float-complex-float-cs-skip.rkt 20:6 -0.9263371220283309 -- float in complex ops
-TR opt: float-complex-float-cs-skip.rkt 21:0 (/ 2.3454025 (flmin (real->double-flonum 1.797693134862315e+308) (real->double-flonum -1.2848677e+32)) (make-rectangular +nan.0 0.0)) -- unboxed binary float complex
-TR opt: float-complex-float-cs-skip.rkt 21:105 (make-rectangular +nan.0 0.0) -- make-rectangular elimination
+TR opt: float-complex-float-cs-skip.rkt 21:105 (make-rectangular +nan.0 0.0) -- binary float comp
 TR opt: float-complex-float-cs-skip.rkt 21:15 (flmin (real->double-flonum 1.797693134862315e+308) (real->double-flonum -1.2848677e+32)) -- binary float
-TR opt: float-complex-float-cs-skip.rkt 21:15 (flmin (real->double-flonum 1.797693134862315e+308) (real->double-flonum -1.2848677e+32)) -- float in complex ops
 TR opt: float-complex-float-cs-skip.rkt 21:22 (real->double-flonum 1.797693134862315e+308) -- float to float
-TR opt: float-complex-float-cs-skip.rkt 21:3 2.3454025 -- float in complex ops
 TR opt: float-complex-float-cs-skip.rkt 21:67 (real->double-flonum -1.2848677e+32) -- float to float
 TR opt: float-complex-float-cs-skip.rkt 22:3 (+ (make-polar 4.8063810141303426e-57 -1.9082319) -0.8414709848078965) -- unboxed binary float complex
 TR opt: float-complex-float-cs-skip.rkt 22:55 -0.8414709848078965 -- float in complex ops
@@ -44,24 +39,13 @@ TR opt: float-complex-float-cs-skip.rkt 24:28 (make-rectangular 3 0.0) -- make-r
 TR opt: float-complex-float-cs-skip.rkt 24:3 (* (min 3/4) 0.9845773 (make-rectangular 3 0.0)) -- unboxed binary float complex
 TR opt: float-complex-float-cs-skip.rkt 24:6 (min 3/4) -- non float complex in complex ops
 TR opt: float-complex-float-cs-skip.rkt 24:6 (min 3/4) -- unary number
-TR opt: float-complex-float-cs-skip.rkt 25:20 (floor -2.2441852) -- float in complex ops
 TR opt: float-complex-float-cs-skip.rkt 25:20 (floor -2.2441852) -- unary float
-TR opt: float-complex-float-cs-skip.rkt 25:3 (/ 3.2993203e+37 (floor -2.2441852) (make-polar 0.42484267570553375 4.940078147009648)) -- unboxed binary float complex
+TR opt: float-complex-float-cs-skip.rkt 25:41 (make-polar 0.42484267570553375 4.940078147009648) -- make-polar
 TR opt: float-complex-float-cs-skip.rkt 25:41 (make-polar 0.42484267570553375 4.940078147009648) -- make-rectangular elimination
-TR opt: float-complex-float-cs-skip.rkt 25:6 3.2993203e+37 -- float in complex ops
-TR opt: float-complex-float-cs-skip.rkt 26:0 (/ -5 2/7 (make-polar -0.0 (fltan (real->double-flonum -3.833043e+21)))) -- unboxed binary float complex
 TR opt: float-complex-float-cs-skip.rkt 26:10 (make-polar -0.0 (fltan (real->double-flonum -3.833043e+21))) -- make-rectangular elimination
+TR opt: float-complex-float-cs-skip.rkt 26:10 (make-polar -0.0 (fltan (real->double-flonum -3.833043e+21))) -- make-polar
 TR opt: float-complex-float-cs-skip.rkt 26:27 (fltan (real->double-flonum -3.833043e+21)) -- unary float
-TR opt: float-complex-float-cs-skip.rkt 26:3 -5 -- non float complex in complex ops
 TR opt: float-complex-float-cs-skip.rkt 26:34 (real->double-flonum -3.833043e+21) -- float to float
-TR opt: float-complex-float-cs-skip.rkt 26:6 2/7 -- non float complex in complex ops
-TR opt: float-complex-float-cs-skip.rkt 27:0 (/ (+ (exact-round 1.8655746e+35) (exact-round 1)) 2.0324421e-21 (make-rectangular 4 1.7976931348623157e+308)) -- unboxed binary float complex
-TR opt: float-complex-float-cs-skip.rkt 27:3 (+ (exact-round 1.8655746e+35) (exact-round 1)) -- non float complex in complex ops
-TR opt: float-complex-float-cs-skip.rkt 27:34 (exact-round 1) -- non float complex in complex ops
-TR opt: float-complex-float-cs-skip.rkt 27:51 2.0324421e-21 -- float in complex ops
-TR opt: float-complex-float-cs-skip.rkt 27:6 (exact-round 1.8655746e+35) -- non float complex in complex ops
-TR opt: float-complex-float-cs-skip.rkt 27:6 (exact-round 1.8655746e+35) -- non float complex in complex ops
-TR opt: float-complex-float-cs-skip.rkt 27:65 (make-rectangular 4 1.7976931348623157e+308) -- make-rectangular elimination
 TR opt: float-complex-float-cs-skip.rkt 28:0 (+ +inf.0-0.0i +nan.0) -- unboxed binary float complex
 TR opt: float-complex-float-cs-skip.rkt 28:15 +nan.0 -- float in complex ops
 TR opt: float-complex-float-cs-skip.rkt 28:3 +inf.0-0.0i -- unboxed literal
@@ -102,13 +86,13 @@ END
 -4.0-2.0i
 +nan.0+0.0i
 -inf.0 -1.06884033e+00
-+nan.0+0.0i
++nan.0+nan.0i
 -8.41470985e-01 -4.53533365e-57
 1.5245886e+12 -9.69431936e-01
 2.21529892e+00 0.0
 -5.84330393e+36 -2.52184871e+37
--inf.0+0.0i
-0.0+nan.0i
++nan.0+nan.0i
+0.0-0.0i
 +nan.0-0.0i
 +nan.0-0.0i
 +nan.0-0.0i

6fd10647b14cff7008bcaa80eb1c3bada564697a
Ben Greenman <benjaminlgreenman@gmail.com>
2020-03-18 13:48:13 -0400

    type-contract: add (only-untyped ....) to HashTableTop contract
    - casts to HashTableTop still work b/c they happen in typed code,
    - but untyped code gets an Any-wrapped value
    
    closes a soundness bug

--- typed-racket-test/succeed/hashtabletop-flat-contract.rkt
+++ typed-racket-test/succeed/hashtabletop-flat-contract.rkt
@@ -1,6 +1,17 @@
 #lang typed/racket/base
+(require typed/rackunit)
 
-;; Test that `HashTableTop` generates a flat contract
+;; Test that `HashTableTop` generates a flat contract in cast,
+;;  and a chaperone when exported to untyped code
 
-(define h : HashTableTop (hash))
+(define h : (Mutable-HashTable Symbol String) (make-hash '((A . "A"))))
 (void (cast h HashTableTop))
+
+(module u racket/base
+  (provide f)
+  (define (f x)
+    (hash-set! x 'A 'bad)))
+(require/typed 'u
+  (f (-> HashTableTop Void)))
+
+(check-exn exn:fail:contract? (lambda () (f h)))

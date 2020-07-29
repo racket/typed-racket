37870d376b9651caea395b53540714c2a9b95c07
bdeket <bedeke@hotmail.com>
2020-07-29 15:39:37 +0200

    typo mandatory arguments

--- typed-racket-lib/typed-racket/base-env/base-special-env.rkt
+++ typed-racket-lib/typed-racket/base-env/base-special-env.rkt
@@ -366,11 +366,11 @@
    (-> -Variable-Reference -Module-Path -Resolved-Module-Path)]
   ;; in-fxvector, in-flvector, in-extflvector
   [(make-template-identifier 'in-fxvector* 'racket/fixnum)
-   (->opt (-FxVector) [-Int (-opt -Int) -Int] (-seq -Fixnum))]
+   (->opt -FxVector [-Int (-opt -Int) -Int] (-seq -Fixnum))]
   [(make-template-identifier 'in-flvector* 'racket/flonum)
-   (->opt (-FlVector) [-Int (-opt -Int) -Int] (-seq -Flonum))]
+   (->opt -FlVector [-Int (-opt -Int) -Int] (-seq -Flonum))]
   [(make-template-identifier 'in-extflvector* 'racket/extflonum)
-   (->opt (-ExtFlVector) [-Int (-opt -Int) -Int] (-seq -ExtFlonum))]
+   (->opt -ExtFlVector [-Int (-opt -Int) -Int] (-seq -ExtFlonum))]
   [(make-template-identifier 'make-namespace-anchor 'racket/private/namespace)
    (-> -Variable-Reference -Namespace-Anchor)]
   [(make-template-identifier 'check-logger-or-false 'racket/private/logger)

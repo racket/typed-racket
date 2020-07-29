62b829056a8a554c155ebb6795f62d593edc8ddc
bdeket <bedeke@hotmail.com>
2020-07-29 15:31:27 +0200

    tests for in-xvector

---
+++ typed-racket-test/succeed/pr953-in-xvector.rkt
@@ -0,0 +1,17 @@
+#lang typed/racket/base
+
+(require racket/sequence
+         racket/flonum
+         racket/fixnum
+         racket/extflonum
+         typed/rackunit)
+
+(check-equal? (sequence->list (in-flvector (flvector 1.0 2.0 3.0 4.0))) (list 1.0 2.0 3.0 4.0))
+(check-equal? (sequence->list (in-flvector (flvector 1.0 2.0 3.0 4.0) 1)) (list 2.0 3.0 4.0))
+(check-equal? (sequence->list (in-flvector (flvector 1.0 2.0 3.0 4.0) 1 2)) (list 2.0))
+(check-equal? (sequence->list (in-flvector (flvector 1.0 2.0 3.0 4.0) 0 #f 2)) (list 1.0 3.0))
+(check-equal? (sequence->list (in-flvector (flvector 1.0 2.0 3.0 4.0) 3 -1 -2)) (list 4.0 2.0))
+
+(check-equal? (sequence->list (in-fxvector (fxvector 1 2 3 4) 3 -1 -2)) (list 4.0 2.0))
+
+(check-equal? (sequence->list (in-extflvector (extflvector 1.0t0 2.0t0 3.0t0 4.0t0) 3 -1 -2)) (list 4.0t0 2.0t0))

34aaa9d68dd25b5f75ab5aa4a8d1bb71863748e4
Fred Fu <moonsolo@gmail.com>
2021-02-24 17:26:12 -0500

    fix subtyping between struct descriptor types & has-struct-property
    a struct descriptor associcated with a property name should be a valid argument
    to its property accessor

--- typed-racket-test/succeed/structs-has-subtype.rkt
+++ typed-racket-test/succeed/structs-has-subtype.rkt
@@ -6,17 +6,16 @@
 (: prop-ins-to-num-ref (Some (X) (-> (Has-Struct-Property prop-ins-to-num) (-> X Number) : X) ))
 (define-values (prop-ins-to-num prop-ins-to-num? prop-ins-to-num-ref) (make-struct-type-property 'prop-ins-to-num))
 
-;; (: bar? : Any -> Boolean : (Has-Struct-Property prop-ins-to-num))
-;; (define bar? prop-ins-to-num?)
 
-
-; (struct (X Y) helloworld ([x : Y] [y : Y]) #:property prop-ins-to-num (cons 20 40))
-
-(struct posn ([x : Integer] [y : Integer])  #:property prop-ins-to-num (λ ([self : posn])
-                                                                         20))
+(struct posn ([x : Integer] [y : Integer])
+  #:property prop-ins-to-num
+  (λ ([self : posn])
+    20))
 
 (: p1 posn)
 (define p1 (posn 100 200))
 (posn-x p1)
+
+(prop-ins-to-num-ref struct:posn)
 (: val Number)
 (define val ((prop-ins-to-num-ref p1) p1))

af8c5a3c344b99925b3e26b6b291a4a135f6aa0f
Fred Fu <moonsolo@gmail.com>
2021-05-09 21:37:38 -0400

    use <line>:<col> instead of <line>.<col> in exn-pred (#1092)
    see https://github.com/racket/racket/commit/808ea5f3034549695ec5756eac0ac44b644c29f6

--- typed-racket-test/fail/cast-mod1.rkt
+++ typed-racket-test/fail/cast-mod1.rkt
@@ -1,5 +1,5 @@
 #;
-(exn-pred exn:fail:contract? #rx".*produced: 3" #rx".*promised: string?.*" #rx"6\\.0")
+(exn-pred exn:fail:contract? #rx".*produced: 3" #rx".*promised: string?.*" #rx"6\\:0")
 
 #lang typed/racket/base
 

38da1eefe9bea5d90e28f6043d5020941b5556bf
Fred Fu <moonsolo@gmail.com>
2021-06-02 14:49:30 -0400

    add <Rep-Name>-update
    This per-rep macro is aimed to ease update on nested fields of rep instances

--- typed-racket-lib/typed-racket/rep/type-rep.rkt
+++ typed-racket-lib/typed-racket/rep/type-rep.rkt
@@ -33,6 +33,8 @@
                                    "base-type-rep.rkt"
                                    "base-union.rkt")
                      Type Prop Object PathElem SomeValues)
+         Struct
+         fld
          Type?
          Mu-maybe-name:
          Vector: Vector?

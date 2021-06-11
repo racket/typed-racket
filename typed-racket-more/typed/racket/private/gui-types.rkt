a07db608b64e82cd74a254ad85a2239af1147812
Lazerbeak12345 <nfblaster@live.com>
2021-06-11 11:06:25 -0600

    Add augments for `on-reorder` and `on-close-request`
    A while ago I added the `on-reorder` and `on-close-request` functions to the `Tab-Panel%` type, but I at the time didn't know how `augment` worked so I didn't actually add them correctly. This should fix this problem.

--- typed-racket-more/typed/racket/private/gui-types.rkt
+++ typed-racket-more/typed/racket/private/gui-types.rkt
@@ -1611,7 +1611,9 @@
          [get-selection (-> (Option Natural))]
          [set ((Listof String) -> Void)]
          [set-item-label (Integer String -> Void)]
-         [set-selection (Integer -> Void)]))
+         [set-selection (Integer -> Void)]
+         (augment [on-reorder ((Listof Exact-Nonnegative-Integer) -> Void)]
+                  [on-close-request (Exact-Nonnegative-Integer -> Void)])))
 
 (define-type Control<%>
   (Class #:implements Subwindow<%>

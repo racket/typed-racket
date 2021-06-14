e9259602dde996088bae59f59a138327f9501986
Fred Fu <moonsolo@gmail.com>
2021-06-14 16:04:14 -0400

    fix variable names and format code

--- typed-racket-test/gui/succeed/test-tab-augments.rkt
+++ typed-racket-test/gui/succeed/test-tab-augments.rkt
@@ -1,10 +1,12 @@
 #lang typed/racket/gui
 
 (define frame (new frame% [label "Example"]))
+
 (define-type Tab-Panel-With-Callbacks%
   (Class #:implements/inits Tab-Panel%
          (init-field [on-reorder-callback ((Listof Exact-Nonnegative-Integer) -> Void)]
                      [on-close-request-callback (Exact-Nonnegative-Integer -> Void)])))
+
 (define tab-panel-with-callbacks% : Tab-Panel-With-Callbacks%
   (class tab-panel%
     (super-new)
@@ -15,6 +17,7 @@
     (define/override (on-close-request index)
       (super on-close-request index)
       (on-close-request-callback index))))
+
 (define tabs : (Instance Tab-Panel-With-Callbacks%)
   (new tab-panel-with-callbacks%
        [choices '("one" "two" "three")]
@@ -23,5 +26,5 @@
        [on-reorder-callback (lambda (tab-list)
                               (displayln (format "on-reorder ~v" tab-list)))]
        [on-close-request-callback
-        (lambda (tabs)
-          (displayln (format "on-close-request ~v" tabs)))]))
+        (lambda (index)
+          (displayln (format "on-close-request ~v" index)))]))

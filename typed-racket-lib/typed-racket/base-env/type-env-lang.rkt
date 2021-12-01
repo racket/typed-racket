bf196a3614302993c448c0d4757e56189047e8d1
Fred Fu <moonsolo@gmail.com>
2021-12-01 12:28:00 -0500

    fix: use aliases for one type(constructor) instead of bindings (#1169)

--- typed-racket-lib/typed-racket/base-env/type-env-lang.rkt
+++ typed-racket-lib/typed-racket/base-env/type-env-lang.rkt
@@ -2,12 +2,21 @@
 
 (require (for-syntax racket/base syntax/parse/pre syntax/stx))
 
+(begin-for-syntax
+  (define-syntax-class t-name
+    #:attributes (nm ty export)
+    (pattern (nm:id ty)
+             #:attr export #'(provide nm))
+    (pattern (nm:id ty #:alias (alias ...))
+             #:attr export #'(provide nm (rename-out [nm alias] ...)))))
+
 (define-syntax (#%module-begin stx)
-  (syntax-parse stx #:literals (require)
-    [(mb (require . args) ... [nm:id ty] ...)
+  (syntax-parse stx
+    #:literals (require)
+    [(mb (require . args) ... name:t-name ...)
      #'(#%plain-module-begin
         (begin
-          (define-syntax (nm stx)
+          (define-syntax (name.nm stx)
             (raise-syntax-error
              'type-check
              (format "type name used out of context\n  type: ~a\n  in: ~a"
@@ -16,7 +25,7 @@
              stx
              (and (stx-pair? stx) (stx-car stx))))
           ...
-          (provide nm) ...
+          name.export ...
           (begin-for-syntax
             (module* initialize #f
               (require
@@ -25,7 +34,7 @@
               (provide initialize-type-names)
               (define (initialize-type-names)
                 (initialize-type-name-env
-                 (list (list #'nm ty) ...)))))))]))
+                 (list (list #'name.nm name.ty) ...)))))))]))
 
 (provide #%module-begin require
          (all-from-out racket/base)

e096c4b6492b39b2df755d06025e586e19eac860
NoahStoryM <noahstorym@gmail.com>
2022-04-13 19:25:54 +0800

    Let `typed/racket/no-check` export `racket/base` at phase level 1.

--- typed-racket-compatibility/typed/scheme/no-check.rkt
+++ typed-racket-compatibility/typed/scheme/no-check.rkt
@@ -1,4 +1,6 @@
 #lang typed-racket/minimal
 
-(require racket/require typed/racket/no-check (subtract-in typed/scheme typed/racket/no-check))
-(provide (all-from-out typed/scheme typed/racket/no-check))
+(require racket/require typed/racket/no-check (subtract-in typed/scheme typed/racket/no-check)
+   (for-syntax scheme/base))
+(provide (all-from-out typed/scheme typed/racket/no-check)
+   (for-syntax (all-from-out scheme/base)))

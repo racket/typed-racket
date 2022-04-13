473d0d444ad7852a423c02b2dc90f5afb8c5ee1c
NoahStoryM <noahstorym@gmail.com>
2022-04-13 20:11:33 +0800

    Update.

--- typed-racket-compatibility/typed/scheme/no-check.rkt
+++ typed-racket-compatibility/typed/scheme/no-check.rkt
@@ -1,7 +1,7 @@
 #lang typed-racket/minimal
 
 (require racket/require typed/private/no-check-helper
-         (subtract-in typed/scheme typed/racket/no-check)
+         (subtract-in typed/scheme typed/private/no-check-helper)
    (for-syntax scheme/base))
 (provide (all-from-out typed/scheme typed/private/no-check-helper)
    (for-syntax (all-from-out scheme/base)))

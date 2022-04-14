7936fc85c1482214dd5a21e6267f26675f92fdbc
NoahStoryM <noahstorym@gmail.com>
2022-04-14 09:46:00 +0800

    Use `typed-scheme` instead of `typed/scheme/base`.

--- typed-racket-compatibility/typed-scheme/no-check.rkt
+++ typed-racket-compatibility/typed-scheme/no-check.rkt
@@ -1,5 +1,5 @@
 #lang typed-racket/minimal
 
 (require racket/require typed/private/no-check-helper
-         (subtract-in typed/scheme/base typed/private/no-check-helper))
-(provide (all-from-out typed/scheme/base typed/private/no-check-helper))
+         (subtract-in typed-scheme typed/private/no-check-helper))
+(provide (all-from-out typed-scheme typed/private/no-check-helper))

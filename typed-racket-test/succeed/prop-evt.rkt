b82489b9e497976c97537788d2bb1d1a55bad90c
Fred Fu <moonsolo@gmail.com>
2022-04-20 20:03:23 -0400

    fix test

--- typed-racket-test/succeed/prop-evt.rkt
+++ typed-racket-test/succeed/prop-evt.rkt
@@ -1,24 +1,39 @@
 #lang typed/racket/base
 
+(define ch ((inst make-channel Number)))
+
 
 (struct aaa0 ((evt : (Evtof Number)))
   #:property prop:evt (struct-field-index evt))
 
-(ann (sync (aaa0 (make-channel))) Number)
+(thread (lambda ()
+          (channel-put ch 10)))
+
+(ann (sync (aaa0 ch)) Number)
 
 
 (struct aaa1 ([evt : (Evtof Number)])
   #:property prop:evt 0)
 
-(ann (sync (aaa1 (make-channel))) Number)
+(thread (lambda ()
+          (channel-put ch 10)))
+
+(ann (sync (aaa1 ch)) Number)
 
 (struct aaa2 ([evt : (Evtof Number)])
   #:property prop:evt (lambda ([self : aaa2]) : (Evtof Number)
                         (aaa2-evt self)))
 
-(ann (sync (aaa2 (make-channel))) Number)
+(thread (lambda ()
+          (channel-put ch 10)))
+(ann (sync (aaa2 ch)) Number)
 
-(struct aaa3 ([evt : (Evtof String)])
-  #:property prop:evt (ann (make-channel) (Evtof String)))
 
-(ann (sync (aaa3 (make-channel))) String)
+(define ch2 ((inst make-channel String)))
+(struct aaa3 ()
+  #:property prop:evt (ann ch2 (Evtof String)))
+
+(thread (lambda ()
+          (channel-put ch2 "10")))
+
+(ann (sync (aaa3)) String)

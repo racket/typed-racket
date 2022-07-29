20acc4bc64907ccf0638c275dccec48461749979
Fred Fu <moonsolo@gmail.com>
2022-07-29 11:25:29 -0400

    tests: improve the check for long-running tests

--- typed-racket-test/send-places.rkt
+++ typed-racket-test/send-places.rkt
@@ -10,12 +10,13 @@
 
 (define-values (enq-ch deq-ch) (place-channel))
 (define (start-workers)
+  (define tm (timeout))
   (when (places)
     (define ws (for/list ([i (places)])
                  (start-worker deq-ch i)))
     (place/context me
       (let loop ([tests (set)])
-        (define val (apply sync/timeout (timeout) ws))
+        (define val (apply sync/timeout tm ws))
         (cond
           [val (define-values (path n _) (split-path val))
                (match n
@@ -24,10 +25,14 @@
                  [_ (loop (if (set-member? tests val)
                               (set-remove tests val)
                               (set-add tests val)))])]
-          [else (unless (zero? (set-count tests))
-                  (eprintf "~a tests are still running:~n~a"
+          [else (unless (set-empty? tests)
+                  ;;  the trailing ~n in the format string forces the standard
+                  ;;  error to flush out. This helps when only 1 test is still
+                  ;;  running.
+                  (eprintf "~a tests are still running:~n~a~n"
                            (set-count tests)
-                           (string-join (set-map tests path->string)
+                           (string-join (set-map tests
+                                                 path->string)
                                         "\n")))
                 (loop tests)])))))
 

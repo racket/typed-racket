d455a506b9e13d81d5aeb08c5041e0ce08580839
Ben Greenman <benjaminlgreenman@gmail.com>
2022-12-12 22:00:59 -0500

    avoid calling normalize-type outside of shallow tr (#1296)

--- typed-racket-lib/typed-racket/utils/shallow-utils.rkt
+++ typed-racket-lib/typed-racket/utils/shallow-utils.rkt
@@ -11,14 +11,17 @@
   (submod typed-racket/rep/type-rep shallow-exports)
   typed-racket/types/tc-result
   typed-racket/types/type-table
-  typed-racket/types/union)
+  typed-racket/types/union
+  typed-racket/utils/tc-utils)
 
 
 (define (ArrowT+? arr)
   (and (Arrow? arr) (Arrow-rng-shallow-safe? arr)))
 
 (define (shallow-trusted-positive? orig-ty)
-  (let loop ((ty (normalize-type orig-ty)))
+  (define in-shallow? (eq? shallow (current-type-enforcement-mode)))
+  ;; 2022-12-12: normalize only in shallow mode to save time
+  (let loop ((ty ((if in-shallow? normalize-type values) orig-ty)))
     (match ty
       [(Fun: arr*)
        (andmap ArrowT+? arr*)]

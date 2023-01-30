e2b143462ae1a67b7e103bab0a9835698a44e3c4
Nathan Fritzler <nfblaster@live.com>
2023-01-29 19:15:21 -0700

    Add the rest-star types

--- typed-racket-more/typed/racket/stream.rkt
+++ typed-racket-more/typed/racket/stream.rkt
@@ -9,32 +9,29 @@
   [#:opaque StreamEmpty stream-empty?]
   [stream-first ((∩ Stream StreamEmpty) -> Any)]
   [stream-rest ((∩ Stream StreamEmpty) -> Stream)]
-  ; TODO macros?
+  ; TODO macros
   ;[stream-cons ...]
   ;[stream-lazy ...]
   [stream-force (Stream -> Stream)]
-  ; TODO macros?
+  ; TODO macros
   ;[stream ...]
   ;[stream* ...]
   [in-stream (Stream -> Stream)]
   ; TODO causes a contract violation
   ;[empty-stream Stream]
   [stream->list (Stream -> (Listof Any))]
-  ; TODO causes a contract violation
   [stream-length (Stream -> Exact-Nonnegative-Integer)]
   [stream-ref (Stream Exact-Nonnegative-Integer -> Any)]
   [stream-tail (Stream Exact-Nonnegative-Integer -> Any)]
   [stream-take (Stream Exact-Nonnegative-Integer -> Any)]
-  ; TODO macros?
-  ;[stream-append (Stream ... -> Any)]
+  [stream-append (->* () #:rest-star (Stream) Any)]
   [stream-map ((Any -> Any) Stream -> Stream)]
-  ; TODO macros?
-  ;[stream-andmap ((Any ... -> Boolean) Stream -> Stream)]
-  ;[stream-ormap ((Any ... -> Boolean) Stream -> Stream)]
-  ;[stream-for-each ((Any ... -> Any) Stream -> Void)]
-  ;[stream-fold ((Any Any ... -> Any) Any Stream -> Any)]
+  [stream-andmap ((->* () #:rest-star (Any) Boolean) Stream -> Stream)]
+  [stream-ormap ((->* () #:rest-star (Any) Boolean) Stream -> Stream)]
+  [stream-for-each ((->* () #:rest-star (Any) Any) Stream -> Void)]
+  [stream-fold ((->* (Any) #:rest-star (Any) Any) Any Stream -> Any)]
   [stream-count ((Any -> Any) Stream -> Exact-Nonnegative-Integer)]
-  ;[stream-filter ((Any ... -> Boolean) Stream -> Stream)]
+  [stream-filter ((->* () #:rest-star (Any) Boolean) Stream -> Stream)]
   [stream-add-between (Stream Any -> Stream)]
   ; TODO for/stream is a a macro
   ; TODO

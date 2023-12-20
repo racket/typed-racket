fed09c739c109bb5320b8032a721ed73bda324ba
Sam Tobin-Hochstadt <samth@indiana.edu>
2023-12-19 22:26:15 -0500

    Repair optimizer log tests for racket/racket#4845.

--- typed-racket-test/optimizer/tests/unboxed-for.rkt
+++ typed-racket-test/optimizer/tests/unboxed-for.rkt
@@ -4,14 +4,11 @@ TR opt: unboxed-for.rkt 2:0 (for/fold: : Float-Complex ((sum : Float-Complex 0.0
 TR opt: unboxed-for.rkt 2:0 (for/fold: : Float-Complex ((sum : Float-Complex 0.0+0.0i)) ((i : Float-Complex (quote (1.0+2.0i 2.0+4.0i)))) (+ i sum)) -- fun -> unboxed fun
 TR opt: unboxed-for.rkt 2:0 (for/fold: : Float-Complex ((sum : Float-Complex 0.0+0.0i)) ((i : Float-Complex (quote (1.0+2.0i 2.0+4.0i)))) (+ i sum)) -- unboxed call site
 TR opt: unboxed-for.rkt 2:0 (for/fold: : Float-Complex ((sum : Float-Complex 0.0+0.0i)) ((i : Float-Complex (quote (1.0+2.0i 2.0+4.0i)))) (+ i sum)) -- unboxed call site
-TR opt: unboxed-for.rkt 2:0 (for/fold: : Float-Complex ((sum : Float-Complex 0.0+0.0i)) ((i : Float-Complex (quote (1.0+2.0i 2.0+4.0i)))) (+ i sum)) -- unboxed let bindings
 TR opt: unboxed-for.rkt 2:0 (for/fold: : Float-Complex ((sum : Float-Complex 0.0+0.0i)) ((i : Float-Complex (quote (1.0+2.0i 2.0+4.0i)))) (+ i sum)) -- unboxed let loop
-TR opt: unboxed-for.rkt 2:31 sum -- leave var unboxed
 TR opt: unboxed-for.rkt 2:31 sum -- unbox float-complex
 TR opt: unboxed-for.rkt 2:31 sum -- unboxed complex variable
 TR opt: unboxed-for.rkt 2:31 sum -- unboxed complex variable
 TR opt: unboxed-for.rkt 2:31 sum -- unboxed var -> table
-TR opt: unboxed-for.rkt 2:53 0.0+0.0i -- unboxed literal
 TR opt: unboxed-for.rkt 3:33 (quote (1.0+2.0i 2.0+4.0i)) -- in-list
 TR opt: unboxed-for.rkt 4:11 sum -- leave var unboxed
 TR opt: unboxed-for.rkt 4:6 (+ i sum) -- unboxed binary float complex
